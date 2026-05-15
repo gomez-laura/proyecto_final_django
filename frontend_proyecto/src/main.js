2784efb1dac1b4a1aaf36f92806180fbf60abb96
Arnau Rubio <arnaugabriel.rubio@ikasle.egibide.org>
2026-05-15 21:36:41 +0200

    Proyecto Vue creado

---
+++ frontend_proyecto/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/bootstrap.scss'
+
+// noinspection ES6UnusedImports
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
